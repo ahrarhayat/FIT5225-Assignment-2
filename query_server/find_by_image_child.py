366de3c147c01270dcb84c20760b91a4677e1dce
Hsuan-Yu-Shih <71647754+Hsuan-Yu-Shih@users.noreply.github.com>
2021-05-21 10:44:40 +1000

    find_by_tag finish, find_by_image not yet

--- query_server/find_by_image_child.py
+++ query_server/find_by_image_child.py
@@ -1,9 +1,119 @@
 import json
+import os
+from datetime import time
+
 import boto3
 import base64
-
 import numpy as np
 import cv2
+
+# construct the argument parse and parse the arguments
+confthres = 0.3
+nmsthres = 0.1
+
+
+def get_labels(labels_path):
+    lpath = os.path.sep.join([yolo_path, labels_path])
+
+    print(yolo_path)
+    LABELS = open(lpath).read().strip().split("\n")
+    return LABELS
+
+
+def get_weights(weights_path):
+    weightsPath = os.path.sep.join([yolo_path, weights_path])
+    return weightsPath
+
+
+def get_config(config_path):
+    configPath = os.path.sep.join([yolo_path, config_path])
+    return configPath
+
+
+def load_model(configpath, weightspath):
+    # load our YOLO object detector trained on COCO dataset (80 classes)
+    print("[INFO] loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet(configpath, weightspath)
+    return net
+
+
+def detect_object(image, net, LABELS):
+    (H, W) = image.shape[:2]
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+
+    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            if confidence > confthres:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                boxes.append([x, y, int(width), int(height)])
+
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confthres,
+                            nmsthres)
+
+    detected_objects = []
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            detected_object = {}
+
+            rectangle = {
+                'height': boxes[i][3],
+                'left': boxes[i][0],
+                'top': boxes[i][1],
+                'width': boxes[i][2]
+            }
+
+            detected_object = {
+                'label': LABELS[classIDs[i]],
+                'accuracy': confidences[i],
+                'rectangle': rectangle
+            }
+
+            detected_objects.append(detected_object)
+    return detected_objects
+
+
+def get_urls(response):
+    url_list = []
+    for item in response['Items']:
+        url_list.append(item['url']['S'])
+    return url_list
+
+
+# This path must be changed to s3 reference
+yolo_path = 'yolo_tiny_configs'
+
+labelsPath = "coco.names"
+cfgpath = "yolov3-tiny.cfg"
+wpath = "yolov3-tiny.weights"
+
+Lables = get_labels(labelsPath)
+CFG = get_config(cfgpath)
+Weights = get_weights(wpath)
 
 dynamodb = boto3.client('dynamodb')
 TABLE_NAME = 'TAG'
@@ -23,26 +133,31 @@ def lambda_handler(event, context):
         image = np_img.copy()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        '''
         nets = load_model(CFG, Weights)  # yolo model
         tags = detect_object(image, nets, Lables)  # invoke detect object function?
-        '''
-        tags = []  # default, no use
+
         tags = list(set(tags))
         url_set = {}
 
+        url_set = set()
+        i = 0
+        tags_num = len(tags)
+
         # assume we have two tables, one is url table, one is tags table
-        for tag in tags:
+        while i < tags_num:
             response = dynamodb.query(
                 TableName=TABLE_NAME,
                 KeyConditionExpression='tag_title = :tag_title',
                 ExpressionAttributeValues={
-                    ':tag_title': {'S': tag}
+                    ':tag_title': {'S': tags[i]}
                 }
             )
-            url_set = url_set & set(response)
 
-        url_list = list(url_set)
+            if i == 0:
+                url_set = set(get_urls(response))
+            else:
+                url_set = url_set & set(get_urls(response))
+            i += 1
 
         return {
             "statusCode": 200,
@@ -50,6 +165,6 @@ def lambda_handler(event, context):
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                "links": url_list
+                "links": list(url_set)
             })
         }
--- query_server/find_by_tag.py
+++ query_server/find_by_tag.py
@@ -6,29 +6,42 @@ dynamodb = boto3.client('dynamodb')
 TABLE_NAME = 'TAG'
 
 
+def get_urls(response):
+    url_list = []
+    for item in response['Items']:
+        url_list.append(item['url']['S'])
+    return url_list
+
+
 def lambda_handler(event, context):
     print(event)
     if event['httpMethod'] == 'POST':
-        data = json.loads(event['body'])   # request have body
+        data = json.loads(event['body'])  # request have body
         tags_encode = data['tags']  # user's tags request
 
-        tags = base64.b64decode(tags_encode)  # list of tags
-        tags = list(set(tags))   # remove repeated tags
+        # tags = base64.b64decode(tags_encode)  # list of tags
 
-        url_set = {}
+        tags = list(set(tags_encode))  # remove repeated tags
+
+        url_set = set()
+        i = 0
+        tags_num = len(tags)
 
         # assume we have two tables, one is url table, one is tags table
-        for tag in tags:
+        while i < tags_num:
             response = dynamodb.query(
                 TableName=TABLE_NAME,
                 KeyConditionExpression='tag_title = :tag_title',
                 ExpressionAttributeValues={
-                    ':tag_title': {'S': tag}
+                    ':tag_title': {'S': tags[i]}
                 }
             )
-            url_set = url_set & set(response)
 
-        url_list = list(url_set)
+            if i == 0:
+                url_set = set(get_urls(response))
+            else:
+                url_set = url_set & set(get_urls(response))
+            i += 1
 
         return {
             "statusCode": 200,
@@ -36,6 +49,14 @@ def lambda_handler(event, context):
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                "links": url_list
+                "links": list(url_set)
             })
         }
+
+
+'''
+{
+  "body": "{\"tags\": [\"cat\", \"person\"]}",
+  "httpMethod": "POST"
+}
+'''
