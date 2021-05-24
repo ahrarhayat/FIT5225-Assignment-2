bb361a085d20215af9b11603b0d3ac2bfcab929e
ahrarhayat <ahrarhayat@gmail.com>
2021-05-24 17:31:11 +1000

    updated object_detection

--- object_detection_lambda.py
+++ object_detection_lambda.py
@@ -1,35 +1,38 @@
 import json
+import base64
 import numpy as np
 import sys
 import time
-import cv2
 import os
+import cv2
 from PIL import Image
 import io
+import boto3
+
 
 # construct the argument parse and parse the arguments
-confthres = 0.3
+confthres = 0.5
 nmsthres = 0.1
+s3 = boto3.client('s3')
 
-
-def get_labels(labels_path):
+#def get_labels(labels_path):
     # load the COCO class labels our YOLO model was trained on
-    lpath = os.path.sep.join([yolo_path, labels_path])
+   # lpath = os.path.sep.join([yolo_path, labels_path])
 
-    print(yolo_path)
-    LABELS = open(lpath).read().strip().split("\n")
-    return LABELS
+  #  print(yolo_path)
+  #  LABELS = open(lpath).read().strip().split("\n")
+  #  return LABELS
 
 
-def get_weights(weights_path):
+#def get_weights(weights_path):
     # derive the paths to the YOLO weights and model configuration
-    weightsPath = os.path.sep.join([yolo_path, weights_path])
-    return weightsPath
+ #   weightsPath = os.path.sep.join([yolo_path, weights_path])
+ #   return weightsPath
 
 
-def get_config(config_path):
-    configPath = os.path.sep.join([yolo_path, config_path])
-    return configPath
+#def get_config(config_path):
+  #  configPath = os.path.sep.join([yolo_path, config_path])
+  #  return configPath
 
 
 def load_model(configpath, weightspath):
@@ -37,8 +40,7 @@ def load_model(configpath, weightspath):
     print("[INFO] loading YOLO from disk...")
     net = cv2.dnn.readNetFromDarknet(configpath, weightspath)
     return net
-
-
+    
 def do_prediction(image, net, LABELS):
     (H, W) = image.shape[:2]
     # determine only the *output* layer names that we need from YOLO
@@ -109,46 +111,73 @@ def do_prediction(image, net, LABELS):
         # loop over the indexes we are keeping
         # create an array of the results
         result = []
-        # loop over the objects and put them in an array and return the result
+        #loop over the objects and put them in an array and return the result
         for i in idxs.flatten():
             result.append({"label": LABELS[classIDs[i]], "accuracy": confidences[i],
                            "rectangle": {"height": boxes[i][0], "left": boxes[i][1], "top": boxes[i][2],
                                          "width": boxes[i][3]}})
         return result
-
-
+        
 ## argument
-# if len(sys.argv) != 2:
-# raise ValueError("Argument list is wrong. Please use the following format:  {} {}".
-#           format("python object_detection.py", "<yolo_config_folder>"))
+#if len(sys.argv) != 2:
+    #raise ValueError("Argument list is wrong. Please use the following format:  {} {}".
+          #           format("python object_detection.py", "<yolo_config_folder>"))
+          
+#This path must be changed to s3 reference
+bucket = 'library-bucket-ahrar'
+key1 = 'coco.names'
+key2 = 'yolov3-tiny.cfg'
+key3 = 'yolov3-tiny.weights'
+s3_resource1 = boto3.resource('s3')
+s3_bucket = s3_resource1.Bucket(bucket)
+s3_obj = s3_bucket.Object(key=key1)
+s3_resource2 = boto3.resource('s3')
+s3_bucket = s3_resource2.Bucket(bucket)
+s3_obj = s3_bucket.Object(key=key2)
+s3_resource3 = boto3.resource('s3')
+s3_bucket = s3_resource3.Bucket(bucket)
+s3_obj = s3_bucket.Object(key=key3)
+        
 
-# This path must be changed to s3 reference
-yolo_path = str(sys.argv[1])
+
+#print(response)
+#yolo_path = str(response)
 
 ## Yolov3-tiny versrion
-labelsPath = "coco.names"
-cfgpath = "yolov3-tiny.cfg"
-wpath = "yolov3-tiny.weights"
-
-Lables = get_labels(labelsPath)
-CFG = get_config(cfgpath)
-Weights = get_weights(wpath)
+#labelsPath = "coco.names"
+#cfgpath = "yolov3-tiny.cfg"
+#wpath = "yolov3-tiny.weights"
 
 
 def lambda_handler(event, context):
+    
+    print('Lamda handler started')
+    
+    
+    # read image which is uplaoded to bucker
+    
+    
+    # read yolo files from bucket 
+    #boto to get file from bucket
+    
+    labels = s3_resource1
+    cfg = s3_resource2
+    weights = s3_resource3
+    
+    # do obejct detection
+    
+    
+    # insert in db
+    
     # TODO implement
-    # image from the s3 bucket
-    image_data = None
-    img = Image.open(io.BytesIO(image_data))
-    # now that the image is extracted, use the same methods as before and use the methods to get the objects
-    # img = cv2.imread(filename)
-    npimg = np.array(img)
-    image = npimg.copy()
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # load the neural net.  Should be local to this method as its multi-threaded endpoint
-    nets = load_model(CFG, Weights)
-    result = do_prediction(image, nets, Lables)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    #image from the s3 bucket
+    #img = Image.open(io.BytesIO(image_data))
+        # now that the image is extracted, use the same methods as before and use the methods to get the objects
+        # img = cv2.imread(filename)
+    #npimg = np.array(img)
+    #image = npimg.copy()
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # load the neural net.  Should be local to this method as its multi-threaded endpoint
+    #nets = load_model(CFG, Weights)
+    #result = do_prediction(image, nets, Lables)
+    
